fec24888b0b802b4bf5a965d216f059d79dfcffb
mofodox <khairulakmal.dev@gmail.com>
2022-10-14 11:44:46 +0800

    create boilerplate with health check endpoint

---
+++ cmd/api/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const VERSION = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development | staging | production")
+	flag.Parse()
+
+	// Initialise a new logger
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	// Initialise an instance of the application struct
+	app := application{
+		config: cfg,
+		logger: logger,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api/v1/healthcheck", app.healthcheckHandler)
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	// Start the HTTP server
+	logger.Printf("%s server is running on port %s\n", cfg.env, srv.Addr)
+	err := srv.ListenAndServe()
+	log.Fatal(err)
+}
