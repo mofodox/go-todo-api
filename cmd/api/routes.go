5282c8dded2329032290b43c57b7ab2306750cdc
mofodox <khairulakmal.dev@gmail.com>
2022-10-15 18:53:22 +0800

    fix: use custom not found and method not allowed functions

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,6 +10,10 @@ func (app *application) routes() *httprouter.Router {
 	// Initialise httprouter router instance
 	router := httprouter.New()
 
+	// Using our custom not found and method not allowed functions
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	router.HandlerFunc(http.MethodGet, "/api/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/api/v1/todos", app.createTodoHandler)
 	router.HandlerFunc(http.MethodGet, "/api/v1/todos/:id", app.showTodoHandler)
