fa58ceced6e2d202fe99aa511b4a15ead96ff724
mofodox <khairulakmal.dev@gmail.com>
2022-10-17 13:31:37 +0800

    fix: add go struct validation

--- internal/data/todos.go
+++ internal/data/todos.go
@@ -4,8 +4,8 @@ import "time"
 
 type Todo struct {
 	ID          int64     `json:"id"`
-	Title       string    `json:"title"`
-	IsCompleted bool      `json:"is_completed"`
+	Title       string    `json:"title" validate:"required"`
+	IsCompleted bool      `json:"is_completed" validate:"required"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"-"`
 	Version     int32     `json:"version"`
