b7233a439204a8339dec0b93ab0d908556ffcf6a
mofodox <khairulakmal.dev@gmail.com>
2022-10-17 14:03:40 +0800

    feat: add db implementation and load env variables

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 const VERSION = "1.0.0"
@@ -14,6 +19,9 @@ const VERSION = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 type application struct {
@@ -24,12 +32,29 @@ type application struct {
 func main() {
 	var cfg config
 
-	flag.IntVar(&cfg.port, "port", 4000, "API server port")
-	flag.StringVar(&cfg.env, "env", "development", "Environment (development | staging | production")
-	flag.Parse()
-
 	// Initialise a new logger
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	if err := godotenv.Load(".env.local"); err != nil {
+		logger.Fatal(err)
+	} else {
+		logger.Println("successfully loaded .env.local")
+	}
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development | staging | production")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("DB_DSN"), "DB DSN")
+	flag.Parse()
+
+	// Initialise DB
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	defer db.Close()
+
+	logger.Println("database connection pool established")
 
 	// Initialise an instance of the application struct
 	app := application{
@@ -47,6 +72,23 @@ func main() {
 
 	// Start the HTTP server
 	logger.Printf("%s server is running on port %s\n", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	log.Fatal(err)
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
