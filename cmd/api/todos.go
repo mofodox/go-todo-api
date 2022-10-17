b762457cf14e059ae9274f406ca1761569e5a24a
mofodox <khairulakmal.dev@gmail.com>
2022-10-17 13:29:53 +0800

    fix: add validation check

--- cmd/api/todos.go
+++ cmd/api/todos.go
@@ -5,19 +5,37 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/mofodox/go-todo/internal/data"
+	// "github.com/mofodox/go-todo/internal/validator"
 )
+
+var v *validator.Validate
 
 func (app *application) createTodoHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Title       string `json:"title"`
-		IsCompleted bool   `json:"is_completed"`
+		Title       string `json:"title" validate:"required"`
+		IsCompleted bool   `json:"is_completed" validate:"required"`
 	}
 
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
 		return
+	}
+
+	todo := &data.Todo{
+		Title:       input.Title,
+		IsCompleted: input.IsCompleted,
+	}
+
+	v = validator.New()
+	err = v.Struct(todo)
+	if err != nil {
+		if e, ok := err.(*validator.InvalidValidationError); ok {
+			app.errorResponse(w, r, http.StatusUnprocessableEntity, e.Error())
+			return
+		}
 	}
 
 	fmt.Fprintf(w, "%+v\n", input)
