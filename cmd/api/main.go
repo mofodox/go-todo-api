2bb73c2932e9f5d4e1505bae3787be69d989d001
mofodox <khairulakmal.dev@gmail.com>
2022-10-18 12:46:20 +0800

    feat: add todo model object

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"github.com/mofodox/go-todo/internal/data"
 )
 
 const VERSION = "1.0.0"
@@ -30,6 +31,7 @@ type config struct {
 type application struct {
 	config config
 	logger *log.Logger
+	models data.Models
 }
 
 func main() {
@@ -66,6 +68,7 @@ func main() {
 	app := application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
 	srv := &http.Server{
